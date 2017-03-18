6569b60fb9dccfb30b8432fdc80c9356df4b9945
Sreeram Padmanabhan <sreeram@L-156095610.local>
2017-03-18 23:49:52 +0000

    Feat: added few material components.

--- src/client/js/components/WeatherApp/WeatherApp.js
+++ src/client/js/components/WeatherApp/WeatherApp.js
@@ -7,6 +7,8 @@ import { Footer } from '../presentational/Footer/Footer';
 import { Loader } from './../presentational/Loader/Loader';
 import { NavBar } from './../presentational/NavBar/NavBar';
 import { MyAppBar } from '../presentational/AppBar/AppBar';
+import Drawer from '../presentational/Drawer/Drawer';
+import Avatar from './../presentational/Avatar/Avatar';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
@@ -21,7 +23,11 @@ injectTapEventPlugin();
 export const WeatherApp = () => (
     <div>
       <MuiThemeProvider>
+        <div>
           <MyAppBar title="Weather" />
+          <Drawer />
+          <Avatar />
+        </div>
       </MuiThemeProvider>
       <NavBar title='Weather'/>
       <main className={Styles.main}>
